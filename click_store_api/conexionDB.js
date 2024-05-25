efb906dcd0cf00260fad161bce32eae588888a93
cmmartinezg <160249987+cmmartinezg@users.noreply.github.com>
2024-05-24 23:03:06 -0400

    conexion

--- click_store_api/conexionDB.js
+++ click_store_api/conexionDB.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgresql://clickStore_owner:AnzBDmGLx80q@ep-jolly-frog-a5sbbxcy-pooler.us-east-2.aws.neon.tech/clickStore?sslmode=require',
+  connectionString: 'postgresql://Click%20Store_owner:zjCRdbrO4cg2@ep-restless-mountain-a5ljzzek.us-east-2.aws.neon.tech/Click%20Store?sslmode=require',
 });
 
 pool.on('connect', () => {
