b50186098a4f55dc289dfc9bd361c441a77338ab
Anai Lugo <anailugo016@gmail.com>
2024-05-25 22:52:15 -0400

    api

--- routes/usuario.js
+++ routes/usuario.js
@@ -1,4 +1,3 @@
-// routes/usuario.js
 const express = require('express');
 const router = express.Router();
 const pool = require('../conexionDB');
@@ -9,6 +8,7 @@ router.get('/', async (req, res) => {
     const result = await pool.query('SELECT * FROM usuarios');
     res.json(result.rows);
   } catch (err) {
+    console.error('Error al obtener usuarios:', err);
     res.status(500).send(err.message);
   }
 });
@@ -16,6 +16,10 @@ router.get('/', async (req, res) => {
 // Crear un nuevo usuario
 router.post('/', async (req, res) => {
   const { nombre, email, contraseña, direccion, telefono } = req.body;
+
+  // Registro para verificar los datos recibidos
+  console.log('Datos recibidos para crear un usuario:', { nombre, email, contraseña, direccion, telefono });
+
   try {
     const result = await pool.query(
       'INSERT INTO usuarios (nombre, email, contraseña, direccion, telefono) VALUES ($1, $2, $3, $4, $5) RETURNING *',
@@ -23,6 +27,7 @@ router.post('/', async (req, res) => {
     );
     res.json(result.rows[0]);
   } catch (err) {
+    console.error('Error al crear un nuevo usuario:', err);
     res.status(500).send(err.message);
   }
 });
