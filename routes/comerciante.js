a765d3a815580531e12d35bb14307dfc5598dbc6
cmmartinezg <160249987+cmmartinezg@users.noreply.github.com>
2024-06-08 13:07:11 -0400

    login

---
+++ routes/comerciante.js
@@ -0,0 +1,34 @@
+// routes/usuario.js
+const express = require('express');
+const router = express.Router();
+const pool = require('../conexionDB');
+
+// Obtener todos los usuarios
+router.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM usuarios');
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+// Crear un nuevo usuario
+router.post('/', async (req, res) => {
+  const { nombre, email, contraseña, direccion, telefono } = req.body;
+  try {
+    const result = await pool.query(
+      'INSERT INTO usuarios (nombre, email, contraseña, direccion, telefono) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+      [nombre, email, contraseña, direccion, telefono]
+    );
+    res.json(result.rows[0]);
+  } catch (err) {
+    if (err.code === '23505') { // Código de error PostgreSQL para violación de restricción de unicidad
+      res.status(409).json({ message: 'El usuario ya existe' });
+    } else {
+      res.status(500).send(err.message);
+    }
+  }
+});
+
+module.exports = router;
---
+++ routes/login.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../conexionDB');
+const bcrypt = require('bcrypt');
+
+// Ruta de login
+router.post('/', async (req, res) => {
+    const { email, contraseña } = req.body;
+
+    // Verificar que el correo electrónico y la contraseña no estén vacíos
+    if (!email || !contraseña) {
+        return res.status(400).json({ error: 'Correo electrónico y contraseña son requeridos.' });
+    }
+
+    try {
+        // Intentar encontrar al usuario en la tabla de usuarios
+        const userResult = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email]);
+        const usuario = userResult.rows[0];
+
+        if (usuario) {
+            // Comparar la contraseña
+            const match = await bcrypt.compare(contraseña, usuario.contraseña);
+            if (match) {
+                res.json({ mensaje: 'Usuario autenticado exitosamente', tipo: 'usuario', usuario });
+                return;
+            } else {
+                res.status(401).json({ error: 'Contraseña incorrecta' });
+                return;
+            }
+        }
+
+        // Intentar encontrar al comerciante en la tabla de comerciantes
+        const merchantResult = await pool.query('SELECT * FROM comerciantes WHERE email = $1', [email]);
+        const comerciante = merchantResult.rows[0];
+
+        if (comerciante) {
+            // Comparar la contraseña
+            const match = await bcrypt.compare(contraseña, comerciante.contraseña);
+            if (match) {
+                res.json({ mensaje: 'Comerciante autenticado exitosamente', tipo: 'comerciante', comerciante });
+                return;
+            } else {
+                res.status(401).json({ error: 'Contraseña incorrecta' });
+                return;
+            }
+        }
+
+        res.status(404).json({ error: 'Usuario o comerciante no encontrado' });
+    } catch (err) {
+        console.error('Login Error:', err);
+        res.status(500).json({ error: 'Error interno del servidor' });
+    }
+});
+
+module.exports = router;
