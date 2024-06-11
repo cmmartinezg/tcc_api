21962601e686883fcfcbb274e441fe86f2746808
cmmartinezg <160249987+cmmartinezg@users.noreply.github.com>
2024-06-10 21:25:11 -0400

    comerciante

--- routes/comerciantes.js
+++ routes/comerciantes.js
@@ -1,34 +1,30 @@
-// routes/usuario.js
 const express = require('express');
 const router = express.Router();
 const pool = require('../conexionDB');
 
-// Obtener todos los usuarios
+// Obtener todos los comerciantes
 router.get('/', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM usuarios');
-    res.json(result.rows);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
+    try {
+        const result = await pool.query('SELECT * FROM comerciantes');
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
-// Crear un nuevo usuario
+// Crear un nuevo comerciante
 router.post('/', async (req, res) => {
-  const { nombre, email, contraseña, direccion, telefono } = req.body;
-  try {
-    const result = await pool.query(
-      'INSERT INTO usuarios (nombre, email, contraseña, direccion, telefono) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-      [nombre, email, contraseña, direccion, telefono]
-    );
-    res.json(result.rows[0]);
-  } catch (err) {
-    if (err.code === '23505') { // Código de error PostgreSQL para violación de restricción de unicidad
-      res.status(409).json({ message: 'El usuario ya existe' });
-    } else {
-      res.status(500).send(err.message);
+    const { nombre, email, contrasena, direccion, telefono, descripcion, enlace_tienda } = req.body;
+    try {
+        const result = await pool.query(
+            'INSERT INTO comerciantes (nombre, email, contrasena, direccion, telefono, descripcion, enlace_tienda) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+            [nombre, email, contrasena, direccion, telefono, descripcion, enlace_tienda]
+        );
+        res.json(result.rows[0]);
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).json({ error: err.message });
     }
-  }
 });
 
 module.exports = router;
