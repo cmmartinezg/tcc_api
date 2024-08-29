1276924677ee1f2378cd0ecd5981d084e82014dc
cmmartinezg <160249987+cmmartinezg@users.noreply.github.com>
2024-08-29 12:33:07 -0400

    prueba

--- openaiConfig.js
+++ openaiConfig.js
@@ -1,10 +1,10 @@
 // Archivo: openaiConfig.js
-const OpenAI = require('openai'); // Importa el paquete completo
-require('dotenv').config(); // Asegúrate de cargar las variables de entorno
+//const OpenAI = require('openai'); // Importa el paquete completo
+//require('dotenv').config(); // Asegúrate de cargar las variables de entorno
 
-// Crea una instancia del cliente de OpenAI
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY, // Usa tu clave API de OpenAI desde las variables de entorno
-});
+//* Crea una instancia del cliente de OpenAI
+//const openai = new OpenAI({
+//  apiKey: process.env.OPENAI_API_KEY, // Usa tu clave API de OpenAI desde las variables de entorno
+//});
 
-module.exports = openai;
+//module.exports = openai;
